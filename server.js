2cd52f37abf4bddeab52e89d69d8404d2c52ff7d
Jared Collier <jared@Jareds-MBP.localdomain>
2022-06-29 18:19:53 -0600

    ready for Heroku, basic page load rollbar message

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const cors = require('cors')
 
 app.use(express.json())
-app.use(cors())
 
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
